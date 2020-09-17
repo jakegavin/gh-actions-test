d8ec08e7a2b0063729a962b0cbb5b18573fb230d
Jake Gavin <jakegavin@gmail.com>
2020-09-17 00:49:50 -0700

    corvallis

--- scripts/fetch-json-data.js
+++ scripts/fetch-json-data.js
@@ -17,3 +17,14 @@ fetch(`https://api.waqi.info/feed/seattle/?token=${waqiToken}`)
     console.error(err);
     process.exit(1);
   });
+
+fetch(`https://api.waqi.info/feed/corvallis/?token=${waqiToken}`)
+  .then((response) => response.json())
+  .then((data) => {
+    const json = JSON.stringify(data, null, "\t");
+    fs.writeFileSync("./data/corvallis-aqi.json", json);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
