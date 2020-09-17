cdd9bdfd876e340c37a977ae209605a3d2f76eef
Jake Gavin <jakegavin@gmail.com>
2020-09-17 10:32:52 -0700

    use handlebars

--- scripts/build-html.js
+++ scripts/build-html.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const Hanldebars = require("handlebars");
 
 const seattleData = require(path.join(__dirname, "../data/seattle-aqi.json"))
   .data;
@@ -7,26 +8,25 @@ const corvallisData = require(path.join(
   __dirname,
   "../data/corvallis-aqi.json"
 )).data;
+const dataForTemplate = {
+  seattle: {
+    aqi: seattleData.aqi,
+    attributions: seattleData.attributions,
+    updatedAt: seattleData.time.iso,
+  },
+  corvallis: {
+    aqi: corvallisData.aqi,
+    attributions: seattleData.attributions,
+    updatedAt: seattleData.time.iso,
+  },
+};
 
-const html = `
-<html>
-  <body>
-    <h3>Air Quality</h3>
-    <div>Seattle: ${seattleData.aqi}</div>
-    <div>
-      Attributions: ${seattleData.attributions
-        .map((source) => `<a href="${source.url}">${source.name}</a>`)
-        .join(", ")}
-    </div>
-    <br/>
-    <div>Corvallis: ${corvallisData.aqi}</div>
-    <div>
-      Attributions: ${corvallisData.attributions
-        .map((source) => `<a href="${source.url}">${source.name}</a>`)
-        .join(", ")}
-    </div>
-  </body>
-</html>
-`;
+const template = fs.readFileSync(
+  path.join(__dirname, "../templates/aqi-hb.html"),
+  { encoding: "utf8", flag: "r" }
+);
+
+const buildHTML = Hanldebars.compile(template);
+const html = buildHTML(dataForTemplate);
 
 fs.writeFileSync(path.join(__dirname, "../docs/index.html"), html);
