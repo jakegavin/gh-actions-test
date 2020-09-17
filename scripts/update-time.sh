08cce2df0a793288552331c6a9b993fea759f25f
Jake Gavin <jakegavin@gmail.com>
2020-09-16 19:27:37 -0700

    f

--- scripts/update-time.sh
+++ scripts/update-time.sh
@@ -1,1 +1,1 @@
-echo "<html><body>Last Build: $(date)<br/>My Secret: $SUPER_SECRET</body><html>" > ./dist/index.html
+echo "<html><body>Last Build: $(date)<br/>My Secret: $SUPER_SECRET</body><html>" > ./docs/index.html
